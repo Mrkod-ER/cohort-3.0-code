1955fe37d3003f3c9e51d5a19254d09abda49ad5
Mrkod-ER <rohitgurjar.khari@gmail.com>
2026-08-07 20:55:01 +0530

    react dynamic post

--- 9.3/9.3-react/src/App.jsx
+++ 9.3/9.3-react/src/App.jsx
@@ -1,31 +1,76 @@
 import { useState } from "react";
-
+import { PostComponent } from "./post";
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+
+  // const posts = [{
+  //   name: "harkirat", 
+  //   subtitle: "1000 followers",
+  //   time: "12 ago", 
+  //   image: "",
+  //   description: "hello"
+  // }]
+
+  const postComponent = posts.map(post => <PostComponent
+    name={post.name}
+    subtitle={post.subtitle}
+    time={post.time}
+    image={post.image}
+    description={post.description}
+    />)
+
+    function addPost() {
+
+      setPosts([...posts, 
+                {
+            name: "harkirat", 
+            subtitle: "1000 followers",
+            time: "12 ago", 
+            image: "",
+            description: "hello"
+          }
+      ])
+      posts.push(
+              {
+          name: "harkirat", 
+          subtitle: "1000 followers",
+          time: "12 ago", 
+          image: "",
+          description: "hello"
+        }
+      )
+    }
+
   return (
     <div>
-      <ToggleMessage/>
-
-    </div>
-  )
-}
-
-const ToggleMessage = () => {
-  const [isVisible, setIsVisible] = useState(false); //defining a new state variable
-  // when the value of a state variable changes, 
-  // the component that uses that state variable rerenders 
-  function toggleMessage() {
-    setIsVisible(!isVisible); 
-  }
-  return (
-    <div>
-      <button onClick={toggleMessage} > 
-          Toggle Message
+      <button onClick={addPost}>
+      add post
       </button>
-      {isVisible && <div> this is conditionally renders </div>}
+      <div>
+        {postComponent}
+      </div>
     </div>
   )
 }
+
+// const ToggleMessage = () => {
+//   const [isVisible, setIsVisible] = useState(false); //defining a new state variable
+//   // when the value of a state variable changes, 
+//   // the component that uses that state variable rerenders 
+//   function toggleMessage() {
+//     setIsVisible(!isVisible); 
+//   }
+//   return (
+//     <div>
+//       <button onClick={toggleMessage} > 
+//           Toggle Message
+//       </button>
+//       {isVisible && <div> this is conditionally renders </div>}
+//     </div>
+//   )
+// }
 
 
 
