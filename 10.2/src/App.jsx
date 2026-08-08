231341a5324f8bad7962a057e4b48978f4880d25
Mrkod-ER <rohitgurjar.khari@gmail.com>
2026-08-08 10:31:32 +0530

    context Api
    understanding context api through light bulb example

---
+++ 10.2/src/App.jsx
@@ -0,0 +1,41 @@
+import './App.css'
+import { useState } from 'react';
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+      <LightBulb />
+    </div>
+    </>
+  )
+}
+function LightBulb() {
+  const [bulbOn, setBulbOn] = useState(true);
+  return <div>
+    <BulbState bulbOn={bulbOn}/>
+    <ToggleBulbState setBulbOn={setBulbOn} />
+  </div>
+}
+
+function BulbState({ bulbOn }) {
+  return <div>
+      {bulbOn ? "Bulb on" : "Bulb off"}
+  </div>
+}
+
+function ToggleBulbState({ setBulbOn }) {
+
+  function toggle() {
+    setBulbOn(currentState => !currentState)
+  }
+  return <div>
+      <button onClick={toggle} >
+        toggle the bulb
+      </button>
+  </div>
+}
+
+export default App
