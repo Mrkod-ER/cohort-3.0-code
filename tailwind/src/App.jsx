cfb054c2cb9f887b1672ca5962ad65efd573b899
Mrkod-ER <rohitgurjar.khari@gmail.com>
2026-08-09 10:54:36 +0530

    responsive grid

--- tailwind/src/App.jsx
+++ tailwind/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   
   return (
     <>
-      <div className="grid grid-cols-12">
+      {/* <div className="grid grid-cols-12">
         <div className='bg-red-200 col-span-5'>
           child 1
         </div>
@@ -13,6 +13,21 @@ function App() {
         </div>
         <div className='bg-green-300 col-span-2'>
           child 2
+        </div>
+      </div>
+      <div className='xl:bg-yellow-500 md:bg-red-700 sm:bg-blue-400 bg-emerald-500'>
+        reponsive div
+      </div> */}
+
+      <div className='grid sm:grid-cols-12 grid-cols-1'>
+        <div className='col-span-1 sm:col-span-5 bg-red-300'>
+          child 1
+        </div>
+        <div className='col-span-1 sm:col-span-5 bg-green-300'>
+          child 1
+        </div>
+        <div className='col-span-1 sm:col-span-2 bg-blue-300'>
+          child 1
         </div>
       </div>
 
